34c507abcabf71aa013cf1cde9a3f925f754d7a9
Girish V <girish.v@needl.ai>
2021-02-28 00:06:21 +0530

    init auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { Navbar } from '../components/Navbar'
+import { Toaster } from 'react-hot-toast'
+import { AuthContextWrapper } from '../lib/context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<AuthContextWrapper>
+			<Navbar />
+			<Component {...pageProps} />
+			<Toaster />
+		</AuthContextWrapper>
+	)
 }
 
 export default MyApp
